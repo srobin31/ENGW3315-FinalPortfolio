e42f7f89c6e200c6b81e2c9f16f6b7434504b021
Shawn Robin <robin.sh@northeastern.edu>
2021-04-14 23:46:12 -0400

    added source code for word clouds and image

---
+++ word_cloud_generator.py
@@ -0,0 +1,52 @@
+import re
+import wordcloud as wc
+import matplotlib.pyplot as plt
+
+files = [
+    'how-is-your-discipline-interdisciplinary',
+    'reflection2',
+    'your-discipline-made-visual',
+    'reflective-self-assessment'
+]
+
+def clean_text(text):
+    ''' Returns the given string in all lowercase without punctuation. '''
+    cleaned_text = []
+    for word in text.lower().split():
+        word = re.sub("[^\w\s]", "", word)
+        cleaned_text.append(word)
+    return ' '.join(cleaned_text)
+
+def generate_wordclouds(data):
+    '''
+    Takes a list of dictionaries storing discussion board posts and produce
+    a wordcloud for each post.
+    '''
+    plt.figure(figsize=(10, 6))
+    index = 1
+    for post in data:
+        cloud = wc.WordCloud().generate(post['clean'])
+        plt.subplot(2, 2, index)
+        plt.title(post['title'])
+        plt.imshow(cloud)
+        plt.axis('off')
+        index += 1
+    plt.show()
+
+def main():
+    data = []
+    for file_name in files:
+        with open('{}.md'.format(file_name)) as file:
+            title = file.readline().strip()
+            post = file.read()
+            data.append({
+                'title': title[4:],
+                'post': post,
+                'clean': clean_text(post)
+            })
+    
+    generate_wordclouds(data)
+
+if __name__ == '__main__':
+    main()
+
